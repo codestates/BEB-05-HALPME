98efd937d7592d073f78012f40a17ce3ae87f973
PARKsuyoun <104402973+PARKsuyoun@users.noreply.github.com>
2022-08-18 17:16:18 +0900

    sample uri 추가

--- client/src/pages/Market.js
+++ client/src/pages/Market.js
@@ -4,6 +4,8 @@ import Web3 from 'web3';
 import erc721Abi from '../assets/abis/erc721Abis';
 
 const nftCA = '0xA1846C81A7a30FB2bA54Bac6232944389F16EC3F';
+const sampleTokenUri1 = 'https://ipfs.io/ipfs/QmVdxTPraJKZskdfFk1kwCWXo6JUwhLRY95M7b1ZUDWxB6?filename=QmVdxTPraJKZskdfFk1kwCWXo6JUwhLRY95M7b1ZUDWxB6';
+const sampleTokenUri2 = 'https://ipfs.io/ipfs/QmcEQdfDrJafpdJVKYPnCF9sW1r3PVt18LiTq8eTR3pCP9?filename=QmcEQdfDrJafpdJVKYPnCF9sW1r3PVt18LiTq8eTR3pCP9';
 
 function Market() {
   const [account, setAccount] = useState('');
@@ -53,6 +55,9 @@ function Market() {
 
   return (
     <div className="Market">
+      <div>NFT Contract Address: {nftCA}</div>
+      <div>sample token URI 1: {sampleTokenUri1}</div>
+      <div>sample token URI 2: {sampleTokenUri2}</div>
       <button className='metaConnect' onClick={() => {
         connectWallet();
       }}>
