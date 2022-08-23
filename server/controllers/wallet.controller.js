dff871ddac932d426691c07f08b1d89c50d8ccaf
HoneyB <qudgus9601@naver.com>
2022-08-23 12:11:03 +0900

    update database

--- server/controllers/wallet.controller.js
+++ server/controllers/wallet.controller.js
@@ -2,13 +2,13 @@ import walletService from "../services/wallet.service";
 
 export default {
   findAll: async (req, res) => {
-    const data = walletService.findAll();
+    const data = await walletService.findAll();
     if (data) {
       res.json({ message: "ok", data: data });
     }
   },
   findByUserId: async (req, res) => {
-    const data = walletService.findByUserId(req.params.id);
+    const data = await walletService.findByUserId(req.params.id);
     if (data) {
       res.json({ message: "ok", data: data });
     }
