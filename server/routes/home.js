12ce91ff9935106ea1422b291bddd078bb906234
hongyu <yuuuj72@gmail.com>
2022-08-25 12:04:32 +0900

    only contract

--- server/routes/home.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import homeController from "../controllers/home.controller";
-
-const router = express.Router();
-
-router.get("/", homeController.getBoard);
-
-export default router;
